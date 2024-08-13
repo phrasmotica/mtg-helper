637fedeca2a193aa3d0cc87da0d521b8fa055aa5
Julian L <julian.lawrance@gmail.com>
2024-08-13 23:22:05 +0100

    Create scene for showing the number of land cards required

---
+++ scripts/land_amount_label.gd
@@ -0,0 +1,24 @@
+@tool
+class_name LandAmountLabel extends HBoxContainer
+
+@export
+var amount: int = 0:
+	set(value):
+		if value >= 0:
+			amount = value
+
+			update_text()
+
+@export
+var land_name := "":
+	set(value):
+		land_name = value
+
+		update_text()
+
+@onready
+var label: Label = %Label
+
+func update_text():
+	if label:
+		label.text = str(amount) + "x " + land_name
