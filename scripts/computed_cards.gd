b0910ce3851219056ec1f31d56476e978b3a872e
Julian Lawrance <julian.lawrance@gmail.com>
2024-08-14 18:18:35 +0100

    Indicate whether there are any basic lands that could not be assigned to a specific mana colour

--- scripts/computed_cards.gd
+++ scripts/computed_cards.gd
@@ -4,11 +4,26 @@ class_name ComputedCards extends VBoxContainer
 @export
 var land_amount_labels := {}
 
+@onready
+var excess_label: Label = %ExcessLabel
+
 var devotion_record: DevotionRecord
 var land_count: int = 0
 
+var total_lands: int = 0:
+	set(value):
+		total_lands = value
+
+		if total_lands < land_count:
+			excess_label.text = "+" + str(land_count - total_lands) + " remaining"
+			excess_label.show()
+		else:
+			excess_label.hide()
+
 func _ready():
 	update_lands()
+
+	excess_label.hide()
 
 func _on_devotion_counts_devotion_changed(dr: DevotionRecord):
 	devotion_record = dr
@@ -22,6 +37,8 @@ func update_lands():
 	if not devotion_record:
 		return
 
+	var computed_lands := 0
+
 	for k in land_amount_labels.keys():
 		var label: LandAmountLabel = get_node(land_amount_labels[k])
 
@@ -29,3 +46,7 @@ func update_lands():
 		var amount = roundi(proportion * land_count)
 
 		label.amount = amount
+
+		computed_lands += amount
+
+	total_lands = computed_lands
