52047e170f3ab6bd7f817cd3a5d63b28bb9a05aa
Julian Lawrance <julian.lawrance@gmail.com>
2024-08-14 13:28:30 +0100

    Use slider instead of spin box for desired land count

--- scripts/land_count.gd
+++ scripts/land_count.gd
@@ -1,7 +1,15 @@
 @tool
 class_name LandCount extends VBoxContainer
 
+@onready
+var amount_label: Label = %AmountLabel
+
 signal count_changed(count: int)
 
 func _on_spin_box_value_changed(value: float):
+	count_changed.emit(int(value))
+
+func _on_h_slider_value_changed(value: float):
+	amount_label.text = str(int(value))
+
 	count_changed.emit(int(value))
